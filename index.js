1a74c4800187d66754e4500725f27bf90c2a6386
Andreas Helmberger <public@ahelmberger.de>
2015-10-15 02:58:57 +0200

    Add healthcheck and instructions for discovery

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 var express = require('express');
 var cors = require('cors');
 
-var SERVICE_PORT = process.env.SERVICE_PORT || 3000;
+// Define some default values if not set in environment
+var PORT = process.env.PORT || 3000;
 var SHUTDOWN_TIMEOUT = process.env.SHUTDOWN_TIMEOUT || 10000;
+var SERVICE_CHECK_HTTP = process.env.SERVICE_CHECK_HTTP || '/healthcheck';
 
+// Create a new express app
 var app = express();
 
+// Add CORS headers
 app.use(cors());
 
+// Add health check endpoint
+app.get(SERVICE_CHECK_HTTP, function (req, res) {
+  res.send({ message: 'OK' });
+});
+
+// Add all other service routes
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-var server = app.listen(SERVICE_PORT);
+// Start the server
+var server = app.listen(PORT);
 
-console.log('Service listening on port ' + SERVICE_PORT + ' ...');
+console.log('Service listening on port %s ...', PORT);
 
 
 
@@ -23,11 +34,14 @@ console.log('Service listening on port ' + SERVICE_PORT + ' ...');
 
 var gracefulShutdown = function () {
   console.log('Received kill signal, shutting down gracefully.');
+
+  // First we try to stop the server smoothly
   server.close(function () {
     console.log('Closed out remaining connections.');
     process.exit();
   });
 
+  // After SHUTDOWN_TIMEOUT we will forcefully kill the process in case it's still running
   setTimeout(function () {
     console.error('Could not close connections in time, forcefully shutting down');
     process.exit();
